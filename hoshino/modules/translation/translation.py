b636c40e49969976f9a66e663a3780d3e2a2bed7
しゃけてり <65025615+Watanabe-Asa@users.noreply.github.com>
2021-01-03 18:19:26 +0800

    Update translation.py

--- hoshino/modules/translation/translation.py
+++ hoshino/modules/translation/translation.py
@@ -3,7 +3,7 @@ import requests
 from hoshino import Service
 from hoshino.typing import *
 
-sv = Service('机器翻译')
+sv = Service('translate')
 
 Headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
 
